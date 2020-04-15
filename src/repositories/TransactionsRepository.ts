c37aa4e19b9cf90b99d1fb27b64edca83a05f69c
João Felipe <joaof.renor@gmail.com>
2020-04-15 16:44:08 -0300

    Challenge Done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income: number = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .map(transactionIncome => transactionIncome.value)
+      .reduce((prev, next) => prev + next, 0);
+    const outcome: number = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .map(transactionOutcome => transactionOutcome.value)
+      .reduce((prev, next) => prev + next, 0);
+    const total: number = income - outcome;
+    return {
+      outcome,
+      income,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
